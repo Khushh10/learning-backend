d8b4a758eff4623cec8778e0e5727b40a944534e
Khush <tatwavedikhush@gmail.com>
2026-05-17 17:13:50 +0530

    Learnt POST and DELETE requests

--- chapter_1/server.js
+++ chapter_1/server.js
@@ -2,20 +2,28 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
-let data = {
-    name: 'Khush'
-}
+let data = ['Khush']
+
+//Middleware
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send(`
         <body>
+        <h1>Data</h1>
         <p>${JSON.stringify(data)}</p>
+        <a href="/dashboard">Go to Dashboard</a>
         </body>
         `)
 })
 
 app.get('/dashboard', (req, res) => {
-    res.send('<h1>Dashboard</h1>')
+    res.send(`
+        <body>
+        <h1>Dashboard</h1>
+        <a href="/">Go back to Home</a>
+        </body>
+        `)
 })
 
 
@@ -24,4 +32,17 @@ app.get('/api/data', (req, res) => {
     res.send(data)
 })
 
+
+app.post('/api/data', (req, res) => {
+    const newEntry = req.body
+    console.log('Received new data:', newEntry)
+    data.push(newEntry.name)
+    res.sendStatus(201)
+})
+
+app.delete('/api/data', (req, res) => {
+    data.pop()
+    console.log('Deleted last entry')
+    res.sendStatus(203)
+})
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!!`))
